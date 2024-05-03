facbbdbff22997b14295517e892753625aec73fc
João Pedro <pedrosant1905@gmail.com>
2024-05-02 23:57:20 -0300

    v0.01

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar/Navbar";
+import styles from "./homepage.module.css";
+import Link from "next/link";
+import Footer from "@/components/footer/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
